99bc16ebef65b16fa3a4f7d1b32818ba1253dcf2
GHE <827997453@qq.com>
2023-04-13 17:58:19 +0800

    feat: 三角形绘制

--- src/Main.ts
+++ src/Main.ts
@@ -13,14 +13,11 @@ class WebGLDemo {
 
         //前缀 a_ 表示变量类型 attribute
         const pos_Key = 'a_Position';
-        const point_Key = 'a_Point';
         //顶点着色器
         const vshader = `
             attribute vec4 ${pos_Key};
-            attribute float ${point_Key};
             void main() {
                 gl_Position = ${pos_Key};
-                gl_PointSize = ${point_Key};
             }
         `;
         //片元着色器
@@ -34,10 +31,9 @@ class WebGLDemo {
         `;
         const program = Utils.i.initShader(gl, vshader, fshader);
         const a_Position = gl.getAttribLocation(program, pos_Key);
-        const a_Point = gl.getAttribLocation(program, point_Key);
         const u_FragColor = gl.getUniformLocation(program, color_Key);
 
-        if (a_Position < 0 || a_Point < 0 || !u_FragColor) {
+        if (a_Position < 0 || !u_FragColor) {
             return;
         }
 
@@ -47,20 +43,19 @@ class WebGLDemo {
             return;
         }
         gl.bindBuffer(gl.ARRAY_BUFFER, vertex_Buffer);
-        const bufData = new Float32Array([0.0, 0.8, -0.8, 0.0, 0.8, 0.0]);
+        const bufData = new Float32Array([0.0, 0.8, -0.8, 0.0, 0.8, 0.0, 1.0, 0.5]);
         gl.bufferData(gl.ARRAY_BUFFER, bufData, gl.STATIC_DRAW);
         gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, true, 0, 0);
         gl.enableVertexAttribArray(a_Position);
 
         //设置点的RGBA
         gl.uniform4f(u_FragColor, Math.random(), Math.random(), Math.random(), Math.random() * 0.5 + 0.5);
-        gl.vertexAttrib1f(a_Point, 20.0);
         
         //设置清除颜色
         gl.clearColor(1.0, 1.0, 1.0, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
 
-        gl.drawArrays(gl.POINTS, 0, 3);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
     }
 }
 WebGLDemo.draw();
