d5a67e43ecede2913bae0dfc4457c3644708fbc7
GHE <827997453@qq.com>
2023-05-08 11:07:57 +0800

    feat: 绘制图形的平移

--- src/Main.ts
+++ src/Main.ts
@@ -13,11 +13,13 @@ class WebGLDemo {
 
         //前缀 a_ 表示变量类型 attribute
         const pos_Key = 'a_Position';
+        const trans_Key = 'u_Trans';
         //顶点着色器
         const vshader = `
             attribute vec4 ${pos_Key};
+            uniform vec4 ${trans_Key};
             void main() {
-                gl_Position = ${pos_Key};
+                gl_Position = ${pos_Key} + ${trans_Key};
             }
         `;
         //片元着色器
@@ -31,6 +33,8 @@ class WebGLDemo {
         `;
         const program = Utils.i.initShader(gl, vshader, fshader);
         const a_Position = gl.getAttribLocation(program, pos_Key);
+        //位置偏移量
+        const u_Trans = gl.getUniformLocation(program, trans_Key);
         const u_FragColor = gl.getUniformLocation(program, color_Key);
 
         if (a_Position < 0 || !u_FragColor) {
@@ -50,6 +54,13 @@ class WebGLDemo {
 
         //设置点的RGBA
         gl.uniform4f(u_FragColor, Math.random(), Math.random(), Math.random(), Math.random() * 0.5 + 0.5);
+        
+        //点击进行绘图的移动
+        canvas.onclick = ev => {
+            gl.uniform4f(u_Trans, 0.2, 0.2, 0.2, 0.0);
+            gl.clear(gl.COLOR_BUFFER_BIT);
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+        }
         
         //设置清除颜色
         gl.clearColor(1.0, 1.0, 1.0, 1.0);
